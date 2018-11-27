f295ae4dee8d407a83c31f8db8c50aaeaceb9d52
Skarpton <anton.skarp@helsinki.fi>
2018-11-28 00:16:52 +0200

    Main class uudelleen ohjaus

---
+++ Pong/src/test/java/BallTest.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pong.Ball;
+
+/**
+ *
+ * @author skarp
+ */
+public class BallTest {
+    private Ball ball;
+    public BallTest() {
+    }
+    
+    
+    @Before
+    public void setUp() {
+        ball = new Ball();
+    }
+    
+    
+}
