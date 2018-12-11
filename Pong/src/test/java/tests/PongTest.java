a04d26a84c664de53e665970eaa5827acf0f092a
Skarpton <anton.skarp@helsinki.fi>
2018-12-11 23:55:31 +0200

    testin lisäys

--- Pong/src/test/java/tests/PongTest.java
+++ Pong/src/test/java/tests/PongTest.java
@@ -5,10 +5,12 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import pong.game.Pong;
+import pong.game.Speeds;
 
 
 public class PongTest {
-    
+    private Pong pong;
     public PongTest() {
     }
     
@@ -18,6 +20,15 @@ public class PongTest {
     
     @Before
     public void setUp() {
+        pong = new Pong(Speeds.getNormal(), true, null);
     }
     
+    @Test
+    public void scoreGoalTest() {
+        for( int i = 0; i < 10000; i++) {
+            pong.move();
+        }
+        pong.scored();
+        assertTrue(pong.getPlayerOneScore() == 1 || pong.getPlayerTwoScore() == 1);
+    }
 }
